7a94621d54af23461b0317ce4e5ba49c9a19082b
Morgan Woods <freelightingwmh@gmail.com>
2020-02-12 20:33:44 +0800

    Create main.cpp

---
+++ TapeEquilibrium/main.cpp
@@ -0,0 +1,20 @@
+#include <limits>
+using namespace std;
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    int s = 0;
+    int m = std::numeric_limits<int>::max();
+    int left_sum = 0;
+    
+    for(int i = 0; i < A.size(); i++)
+        s += A[i];
+    
+    
+    for (int i = 0; i < A.size(); i++){
+        left_sum += A[i];
+        m = min(m,abs(s-2*left_sum));
+    }
+    
+    return m;
+    
+}
