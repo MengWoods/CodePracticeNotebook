d15888a0cbe37bef44dfbe8d8b7353c10552b89b
Morgan Woods <freelightingwmh@gmail.com>
2020-02-12 16:49:17 +0800

    Update main.cpp

--- CyclicRotation/main.cpp
+++ CyclicRotation/main.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+//#include <bits/stdc++.h>
+
+using namespace std;
 
 vector<int> solution(vector<int> &A, int K) {
     // write your code in C++14 (g++ 6.2.0)
@@ -10,8 +15,22 @@ vector<int> solution(vector<int> &A, int K) {
     for (int i = 0; i<A.size(); i++){
         res[(i+K)%(A.size())] = A[i];
     }
-    
-    
     return res;
+}
+
+int main() {
+    // insert code here...
     
+    //int *p ;
+    vector<int> A{1,2,3,4,5};
+    int K = 1;
+    
+    //int B[sizeof(A)] = {0};
+    vector<int> res = solution(A,K);
+    
+    for (int i=0; i<res.size();i++){
+        cout<<res[i]<<',';
+    }
+    
+    return 0;
 }
